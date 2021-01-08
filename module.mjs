eb6316e1b23b261ed6c2caeaf003d77040e29c68
Cafe137 <aron@aronsoos.com>
2021-01-08 10:16:10 +0100

    add esm support

---
+++ module.mjs
@@ -0,0 +1,16 @@
+import utility from './index.js'
+
+export const Arrays = utility.Arrays
+export const Assertions = utility.Assertions
+export const Cache = utility.Cache
+export const Dates = utility.Dates
+export const Files = utility.Files
+export const Logger = utility.Logger
+export const Numbers = utility.Numbers
+export const Objects = utility.Objects
+export const Pagination = utility.Pagination
+export const Promises = utility.Promises
+export const Random = utility.Random
+export const Strings = utility.Strings
+export const System = utility.System
+export const Types = utility.Types
